9431e029a4e6da1d72469e9e58d867d5e7d046e0
PriyavasugamaniIMAD <priya.vasugamani@gmail.com>
2016-10-08 13:26:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,8 +85,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res)
 {
     //articlename=article-one;
-    res.send(createTemplate(articleOne));
-    var articleName=params.articleName;
+    var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
     
 });
