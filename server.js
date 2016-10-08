33dfe835ffee3ca594ba6c1f1d5286df626a7937
PriyavasugamaniIMAD <priya.vasugamani@gmail.com>
2016-10-08 11:18:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req,res){
-    res.send('Article One is requested and will be served here')
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
 });
 app.get('/article-two',function(req,res){
-    res.send('Article Two is requested and will be served here')
+    res.send('Article Two is requested and will be served here');
     
 });
 app.get('/article-three',function(req,res){
-    res.send('Article Three is requested and will be served here')
+    res.send('Article Three is requested and will be served here');
     
 });
 
